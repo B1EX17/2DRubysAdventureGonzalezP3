94fe12115945838f8f5e09e7198f20434d491cf7
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-11-16 09:59:56 -0800

    added
    fixed the enemy code

--- 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/EnemyController.cs
+++ 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/EnemyController.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed;
     public bool vertical;
-    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
-    float timer;
-    int direction = 1;
 
     // Start is called before the first frame update
     void Start()
