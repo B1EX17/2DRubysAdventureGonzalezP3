660f0ac5a77ec252e4ad62bbca8d0c2b48d326cd
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-11-17 12:20:52 -0800

    codeds decorations
    I added more coding and decorations made the enemy move everyway

--- 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/EnemyController.cs
+++ 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/EnemyController.cs
@@ -6,13 +6,31 @@ public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
 
     void FixedUpdate()
@@ -21,13 +39,27 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
