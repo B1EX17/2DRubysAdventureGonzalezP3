69bff6bd3d3b975375ff392c9c1c788150409189
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-11-22 18:05:12 -0800

    Added my helth as a prefab
    i frogot to add my health as a prefab so thast what i did

--- 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/Projectile.cs
+++ 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour
 {
 
     Rigidbody2D rigidbody2d;
-    // Start is called before the first frame update
+    
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
