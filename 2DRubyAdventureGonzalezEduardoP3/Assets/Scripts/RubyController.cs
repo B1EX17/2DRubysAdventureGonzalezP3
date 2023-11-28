cd0c635752c73076408f8c719cdb7ecd8e24cc1e
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-11-28 11:40:06 -0800

    Added code
    I finished the code for ruby and the robot so now i can shoot a cog bullet to the robot and make it stop

--- 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/RubyController.cs
+++ 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/RubyController.cs
@@ -7,11 +7,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+
+    public GameObject projectilePrefab;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
@@ -21,7 +23,6 @@ public class RubyController : MonoBehaviour
 
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
-    GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -56,6 +57,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -65,18 +71,12 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Launch();
-        }
     }
 
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
@@ -87,7 +87,8 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-    void Launch() 
+
+    void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
