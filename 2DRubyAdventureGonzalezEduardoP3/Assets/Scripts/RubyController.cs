eef612fb09994694dc8e921ce4c4658601f84b10
Eduardo Gonzalez <145380839+B1EX17@users.noreply.github.com>
2023-11-29 22:24:51 -0800

    Added code and damage health
    I added a UIHealth bar to show on my screen every time i take damage and added more details to my world

--- 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/RubyController.cs
+++ 2DRubyAdventureGonzalezEduardoP3/Assets/Scripts/RubyController.cs
@@ -85,7 +85,7 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetVaule(currentHealth/(float)maxHealth);
     }
 
     void Launch()
